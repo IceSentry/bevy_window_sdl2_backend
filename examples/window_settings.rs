10820fbb21f02f8cf04ca9e500b8f417558ecac2
IceSentry <c.giguere42@gmail.com>
2025-07-07 20:27:25 -0400

    Add 3d_scene example

--- examples/window_settings.rs
+++ examples/window_settings.rs
@@ -2,8 +2,8 @@ use bevy::DefaultPlugins;
 use bevy::app::App;
 use bevy::diagnostic::FrameCount;
 use bevy::prelude::*;
-use bevy_window::{Window, WindowPlugin};
 use bevy::winit::WinitPlugin;
+use bevy_window::{Window, WindowPlugin};
 use bevy_window_sdl2_backend::Sdl2WindowBackendPlugin;
 
 fn main() {
@@ -14,7 +14,7 @@ fn main() {
             DefaultPlugins
                 .set(WindowPlugin {
                     primary_window: Some(Window {
-                        title: "bevy sdl window".into(),
+                        title: "window_settings SDL2".into(),
                         visible: false,
                         ..Default::default()
                     }),
@@ -23,42 +23,8 @@ fn main() {
                 // Make sure to disable the WinitPlugin
                 .disable::<WinitPlugin>(),
         ))
-        .add_systems(Startup, setup)
         .add_systems(Update, (make_visible, change_title))
         .run();
-}
-
-/// set up a simple 3D scene
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    // circular base
-    commands.spawn((
-        Mesh3d(meshes.add(Circle::new(4.0))),
-        MeshMaterial3d(materials.add(Color::WHITE)),
-        Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
-    ));
-    // cube
-    commands.spawn((
-        Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
-        MeshMaterial3d(materials.add(Color::srgb_u8(124, 144, 255))),
-        Transform::from_xyz(0.0, 0.5, 0.0),
-    ));
-    // light
-    commands.spawn((
-        PointLight {
-            shadows_enabled: true,
-            ..default()
-        },
-        Transform::from_xyz(4.0, 8.0, 4.0),
-    ));
-    // camera
-    commands.spawn((
-        Camera3d::default(),
-        Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
-    ));
 }
 
 fn make_visible(mut window: Single<&mut Window>, frames: Res<FrameCount>) {
