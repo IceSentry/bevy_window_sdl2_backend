1e829edc5b33c1bf8012086976cc28e0841a636d
IceSentry <c.giguere42@gmail.com>
2025-11-18 14:50:51 -0500

    add frame limiter

---
+++ examples/framepace.rs
@@ -0,0 +1,83 @@
+use bevy::{color::palettes, prelude::*, winit::WinitPlugin};
+use bevy_window::{CursorIcon, PresentMode, SystemCursorIcon, Window, WindowResolution};
+use bevy_window_sdl2_backend::{Sdl2FrameLimiter, Sdl2WindowBackendPlugin};
+
+fn main() {
+    App::new()
+        .add_plugins((
+            // Add the plugin
+            Sdl2WindowBackendPlugin,
+            // Make sure to disable the WinitPlugin
+            DefaultPlugins
+                .build()
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "SDL2 framepace".into(),
+                        present_mode: PresentMode::AutoVsync,
+                        resolution: WindowResolution::new(1920, 1080)
+                            .with_scale_factor_override(1.0),
+                        ..Default::default()
+                    }),
+                    ..Default::default()
+                })
+                .disable::<WinitPlugin>(),
+        ))
+        .add_systems(Startup, setup)
+        .add_systems(Update, (toggle_plugin, update_ui, update_cursor))
+        .run();
+}
+
+#[derive(Component)]
+struct EnableText;
+
+fn toggle_plugin(
+    input: Res<ButtonInput<KeyCode>>,
+    mut limiter: ResMut<Sdl2FrameLimiter>,
+    mut window: Single<&mut Window>,
+) {
+    if input.just_pressed(KeyCode::Space) {
+        limiter.enabled = !limiter.enabled;
+    }
+
+    if input.just_pressed(KeyCode::KeyT) {
+        if window.present_mode == PresentMode::AutoVsync {
+            window.present_mode = PresentMode::AutoNoVsync;
+        } else {
+            window.present_mode = PresentMode::AutoVsync;
+        }
+    }
+}
+
+fn update_ui(
+    mut text: Single<&mut TextSpan, With<EnableText>>,
+    limiter: Res<Sdl2FrameLimiter>,
+    window: Single<&Window>,
+) {
+    text.0 = format!("{} pres mode: {:?}", limiter.enabled, window.present_mode);
+}
+
+pub fn update_cursor(window: Single<&Window>, mut gizmos: Gizmos) {
+    if let Some(pos) = window.cursor_position() {
+        let pos = Vec2::new(pos.x - window.width() / 2.0, window.height() / 2.0 - pos.y);
+        gizmos.circle_2d(pos, 10.0, palettes::basic::GREEN);
+    }
+}
+
+/// set up the scene
+fn setup(mut commands: Commands, window: Single<Entity, With<Window>>) {
+    commands
+        .entity(*window)
+        .insert(CursorIcon::System(SystemCursorIcon::Crosshair));
+    commands.spawn(Camera2d);
+
+    // UI
+    let text_font = TextFont {
+        font_size: 50.,
+        ..default()
+    };
+    commands
+        .spawn(Text::default())
+        .with_child((TextSpan::new("Frame pacing: "), text_font.clone()))
+        .with_child((TextSpan::new(""), text_font.clone(), EnableText))
+        .with_child((TextSpan::new("\n[press space]"), text_font));
+}
--- src/lib.rs
+++ src/lib.rs
@@ -39,7 +39,73 @@ impl Plugin for Sdl2WindowBackendPlugin {
         app.set_runner(|app| sdl_runner(app, sdl_context))
             .add_systems(Last, set_cursor)
             .add_systems(Last, changed_bevy_windows);
+
+        app.insert_resource(Sdl2FrameLimiter {
+            enabled: false,
+            render_target: std::time::Instant::now(),
+            target_framerate: None,
+            target_frame_time: None,
+            display_resfresh_rate: 0,
+        });
+
+        app.add_systems(Update, get_display_refresh_rate);
     }
+}
+
+#[derive(Resource)]
+pub struct Sdl2FrameLimiter {
+    pub enabled: bool,
+    render_target: std::time::Instant,
+    target_framerate: Option<i32>,
+    target_frame_time: Option<std::time::Duration>,
+    display_resfresh_rate: i32,
+}
+
+impl Default for Sdl2FrameLimiter {
+    fn default() -> Self {
+        Self {
+            enabled: false,
+            render_target: std::time::Instant::now(),
+            target_framerate: None,
+            target_frame_time: None,
+            display_resfresh_rate: 0,
+        }
+    }
+}
+
+impl Sdl2FrameLimiter {
+    fn set_framerate(&mut self, target_framerate: i32) {
+        self.target_framerate = Some(target_framerate);
+        self.target_frame_time = Some(Duration::from_nanos(
+            1_000_000_000 / target_framerate as u64,
+        ));
+    }
+
+    fn set_display_refresh_rate(&mut self, display_resfresh_rate: i32) {
+        self.display_resfresh_rate = display_resfresh_rate;
+    }
+}
+
+fn get_display_refresh_rate(
+    mut limiter: ResMut<Sdl2FrameLimiter>,
+    windows: Query<Entity, With<Window>>,
+    _marker: NonSendMarker,
+) {
+    let refresh_rate = windows
+        .iter()
+        .filter_map(|e| {
+            SDL_WINDOWS.with_borrow(|windows| {
+                let window = windows.get_window(e)?;
+                let display_mode = window.display_mode().ok()?;
+                Some(display_mode.refresh_rate)
+            })
+        })
+        // TODO not sure if min makes sense here. I think we should look for the window that is
+        // currently active and use the refresh_rate from that
+        .min()
+        .expect("Failed to find refresh rate");
+    limiter.set_framerate(refresh_rate);
+    limiter.set_display_refresh_rate(refresh_rate);
 }
 
 fn sdl_runner(mut app: App, sdl_context: Sdl) -> AppExit {
@@ -64,6 +130,15 @@ fn sdl_runner(mut app: App, sdl_context: Sdl) -> AppExit {
             );
         create_windows(create_window.get_mut(app.world_mut()), &mut video_subsystem);
         create_window.apply(app.world_mut());
+        app.world_mut()
+            .resource_scope(|_world, mut limiter: Mut<Sdl2FrameLimiter>| {
+                if limiter.enabled
+                    && limiter.render_target <= std::time::Instant::now()
+                    && let Some(target_frame_time) = limiter.target_frame_time
+                {
+                    limiter.render_target += target_frame_time;
+                }
+            });
 
         for event in event_pump.poll_iter() {
             match handle_sdl_event(&mut app, event, &mut bevy_window_events) {
@@ -76,6 +151,19 @@ fn sdl_runner(mut app: App, sdl_context: Sdl) -> AppExit {
         }
 
         app.update();
+
+        app.world_mut()
+            .resource_scope(|_world, mut limiter: Mut<Sdl2FrameLimiter>| {
+                if limiter.enabled {
+                    let now = std::time::Instant::now();
+                    if limiter.render_target <= now
+                        && let Some(target_frame_time) = limiter.target_frame_time
+                    {
+                        limiter.render_target = now + target_frame_time;
+                    }
+                    spin_sleep::sleep_until(limiter.render_target);
+                }
+            });
     }
     app.world_mut().clear_all();
     AppExit::Success
